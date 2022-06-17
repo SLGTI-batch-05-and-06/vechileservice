464d0a19ea61cbf841e73d4fc8dba8eba6df8ac9
Thanexan <thanex1998@gmail.com>
2022-06-17 15:34:53 +0530

    upload css

---
+++ include/Blf.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  <?php include('script.php'); ?>
+<!-- <link rel="stylesheet" href="/ss.css"> -->
+</head>
+<body>
+    <footer>
+<div class="mainfooter">
+<div class="row text-white ">
+<div class="col-md-2 p-4 "></div>
+    <div class="col-md-2 p-4 ">
+        <ul type="none" class="text-left ">
+            <li class="fw-bold">Services</li>
+            <li>Vehicle Repair</li>
+            <li>Vehicle Servicing</li>
+            <li>Collision Repair</li>
+            <li>Additional Services</li>
+            <li>24-Hr Mobile First Aid</li>
+            <li>Spare Parts</li>
+        </ul>
+</div>
+    <div class="col-md-2 p-4 ">
+        
+        <ul type="none">
+            <li class="fw-bold">About</li>
+            <li>Our Values</li>
+            <li>About Us</li>
+            <li>Mission</li>
+            <li>Vision</li>
+            <li>Awards and Certifications</li>
+            <li>Careers</li>
+
+        </ul>
+    </div>
+    <div class="col-md-2 p-4 ">
+        
+        <ul type="none">
+           <li class="fw-bold">Media</li>
+           <li>Press Releases</li>
+           <li>Videos</li>
+           <li>Downloads</li>
+           <li>External Links</li>
+        </ul>
+    </div>
+    <div class="col-md-2 p-4 ">
+        <ul type="none">
+            <li class="fw-bold">Informations</li>
+            <li>Terms and Conditions</li>
+            <li>FAQ</li>
+        </ul>
+
+    </div>
+</div>
+</div>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 <html lang="en">
 
 <head>
+    <?php include('include/script.php'); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.css">
-    <link rel="stylesheet" href="ss.css">
+    <title>VTS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
 
-<body>
+<body class="mainbody">
     
     <div class="row mt-5">
         <div class="col-4"></div>
