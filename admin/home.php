fdc6b51b28446852c6d1957a9c956e0207719a1c
Lisha <thanex1998@gmail.com>
2022-07-28 00:15:20 +0530

    updatejul28

--- admin/home.php
+++ admin/home.php
@@ -1,5 +1,9 @@
+<?php
+$conn =  mysqli_connect("localhost","root","","vehicle_service");
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,71 +12,134 @@
     <link rel="stylesheet" href="css/a.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <?php include('../include/script.php'); ?>
+    <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
+
 <body>
-<body>
-    <div class="container"> 
-        <div class="sidebar">
-            <ul>
-                <li>
-                    <a href="">
-                        <i class="fa fa-wrench"></i>
-                        <div class="title">TJS</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-dashboard"></i>
-                        <div class="title">Dashboard</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-user"></i>
-                        <div class="title">Mechanic</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-file-text-o"></i>
-                        <div class="title">Service Request</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-file-o"></i>
-                        <div class="title">Report</div>
-                    </a>
-                </li>
-                <li>
-                        <h4>Maintenance</h4>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-list"></i>
-                        <div class="title">Parts list </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-list"></i>
-                        <div class="title">Service list</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-users"></i>
-                        <div class="title">User list</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-question"></i>
-                        <div class="title">Help</div>
-                    </a>
-                </li>
-            </ul>
+
+    <body>
+        <div class="container">
+            <div class="sidebar">
+                <ul>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-wrench"></i>
+                            <div class="title">TJS</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-dashboard"></i>
+                            <div class="title">Dashboard</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-user"></i>
+                            <div class="title">Mechanic</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-file-text-o"></i>
+                            <div class="title">Service Request</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-file-o"></i>
+                            <div class="title">Report</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-list"></i>
+                            <div class="title">Parts list </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-list"></i>
+                            <div class="title">Service list</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-users"></i>
+                            <div class="title">User list</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-question"></i>
+                            <div class="title">Help</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="main">
+                <div class="top-bar">
+                    <i></i>
+                    <div class="user">
+                        <img src="image/ad.png" alt="">
+                    </div>
+                </div>
+                <div class="cards">
+                    <div class="card bg-secondary">
+                        <div class="card-content">
+                            <div class="count"><?php
+                        $inv=$conn->query("SELECT count(id) as total FROM `parts`")->fetch_assoc()['total'];
+                        echo number_format($inv);
+                        ?>
+                            </div>
+                            <div class="card-name">Total Parts</div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="fa fa-list"></i>
+                        </div>
+                    </div>
+                    <div class="card bg-info">
+                        <div class="card-content">
+                            <div class="count"><?php
+                        $mechanics = $conn->query("SELECT sum(mechanic_id)as total FROM `mechanic` where status='1'")->fetch_assoc()['total'];
+                        echo number_format($mechanics);
+                        ?>
+                            </div>
+                            <div class="card-name">Mechanic</div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="fa fa-user"></i>
+                        </div>
+                    </div>
+                    <div class="card bg-success">
+                        <div class="card-content">
+                            <div class="count"><?php
+                        $services = $conn->query("SELECT sum(service_id)as total FROM `service_list` where status='1'")->fetch_assoc()['total'];
+                        echo number_format($services);
+                        ?>
+                            </div>
+                            <div class="card-name">Services</div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="fa fa-list"></i>
+                        </div>
+                    </div>
+                    <div class="card bg-warning">
+                        <div class="card-content">
+                            <div class="count"><?php
+                        $mechanics = $conn->query("SELECT sum(mechanic_id)as total FROM `mechanic` where status='1'")->fetch_assoc()['total'];
+                        echo number_format($mechanics);
+                        ?>
+                            </div>
+                            <div class="card-name">Finished Request</div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="fa fa-file-text-o"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+
 </html>
